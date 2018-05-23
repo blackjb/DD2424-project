91d57a6459b7b858c212b4f7bc3f6d2a3763fc30
tryti <tryti@kth.se>
2018-05-23 19:59:13 +0200

    Cleanup of models.py

--- models.py
+++ models.py
@@ -14,36 +14,6 @@ def linear_model(input_dims, nb_labels):
                   metrics=['accuracy'])
     return model
 
-def minimal(input_dims, nb_labels):
-    model = models.Sequential()
-    model.add(layers.Dense(input_dims), activation='relu')
-    model.add(layers.Dense(nb_labels, activation='softmax'))
-
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    return model
-
-def conv_model(input_dims, nb_labels):
-    model = models.Sequential()
-
-    model.add(layers.Convolution2D(32, 3, 3, activation='relu', input_shape=input_dims))
-    model.add(layers.Convolution2D(32, 3, 3, activation='relu'))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(layers.Dropout(0.25))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1000, activation='relu'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(nb_labels, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-
-    return model
-
-
 
 def alexnet_model1(input_dims, nb_labels, activation, optimizer, norm=False, dropout=0):
     model = models.Sequential()
@@ -53,14 +23,12 @@ def alexnet_model1(input_dims, nb_labels, activation, optimizer, norm=False, dro
         model.add(layers.BatchNormalization())
     model.add(layers.Activation(activation))
     model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
-    #model.add(layers.MaxPool2D(pool_size=(3, 3), padding='valid'))
 
     model.add(layers.Convolution2D(128, 5, 1))
     if norm:
         model.add(layers.BatchNormalization())
     model.add(layers.Activation(activation))
     model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
-    #model.add(layers.MaxPool2D(pool_size=(3, 3), padding='valid'))
 
     model.add(layers.Convolution2D(192, 3, 1))
 
@@ -69,7 +37,6 @@ def alexnet_model1(input_dims, nb_labels, activation, optimizer, norm=False, dro
     model.add(layers.Convolution2D(128, 3, 1))
     model.add(layers.Activation(activation))
     model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding='valid'))
-    #model.add(layers.MaxPool2D(pool_size=(3, 3), padding='valid'))
 
     #Fully conected end layers
     model.add(layers.Flatten())
@@ -85,6 +52,7 @@ def alexnet_model1(input_dims, nb_labels, activation, optimizer, norm=False, dro
                   metrics=['accuracy'])
 
     return model
+
 
 def alexnet_model2(input_dims, nb_labels, activation, optimizer, norm=False):
     model = models.Sequential()
